9856d28454816a43c450ca65500a947b4d2eefda
oburstein <oburstein@gmail.com>
2013-08-22 16:01:39 -0700

    formatting changes for weather and city

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ $(document).ready (function() {
             url: "http://api.wunderground.com/api/dfc805df5d6fe1d4/geolookup/conditions/q/" + location + ".json",
             dataType: "jsonp",
             success: function (parsed_json) {
-                var location = parsed_json['location']['city'];
+                var city = parsed_json['location']['city'];
                 var temp_f = parsed_json['current_observation']['temp_f'];
                 var image = parsed_json['current_observation']['icon_url'];
-                $('.weather').append("Current temperature in " + location + " is: " + temp_f + " F");
+                $('.weather').append(temp_f + "&deg; F");
 
                 $('#weather_icon').attr("src", image);
+                $('.city').append(city);
             }
         });
         
