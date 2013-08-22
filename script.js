2bfaf8fcb3a0e6f246b338ebf5269f0ade044635
oburstein <oburstein@gmail.com>
2013-08-22 15:47:43 -0700

    browser geolocation added
    note: still need to include method for error scenario

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-jQuery(document).ready(function($) {
-  $.ajax({
-  url : "http://api.wunderground.com/api/dfc805df5d6fe1d4/geolookup/conditions/q/IA/Seattle.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  var image = parsed_json['current_observation']['icon_url'];
-  $('.weather').append("Current temperature in " + location + " is: " + temp_f + " F");
+$(document).ready (function() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var location = position.coords.latitude + ',' + position.coords.longitude;
+        $.ajax({
+            url: "http://api.wunderground.com/api/dfc805df5d6fe1d4/geolookup/conditions/q/" + location + ".json",
+            dataType: "jsonp",
+            success: function (parsed_json) {
+                var location = parsed_json['location']['city'];
+                var temp_f = parsed_json['current_observation']['temp_f'];
+                var image = parsed_json['current_observation']['icon_url'];
+                $('.weather').append("Current temperature in " + location + " is: " + temp_f + " F");
 
-  $('#weather_icon').attr("src", image);
-  }
-  });
+                $('#weather_icon').attr("src", image);
+            }
+        });
+        
+    });
 });
