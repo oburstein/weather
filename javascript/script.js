61553a36b27b0a114d0b2e829dc417bacac51219
oburstein <oburstein@gmail.com>
2013-08-26 12:44:36 -0700

    smaller image and scroll locking function
    also changed icon type to test retrieval of different icon set

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,10 @@
 $(document).ready (function() {
+
+    // attempt to block scrolling up and down. may need to add this: <body ontouchmove="blockMove()">
+    function blockMove() {
+      event.preventDefault() ;
+    }
+
     navigator.geolocation.getCurrentPosition(function (position) {
         var location = position.coords.latitude + ',' + position.coords.longitude;
         $.ajax({
@@ -7,9 +13,13 @@ $(document).ready (function() {
             success: function (parsed_json) {
                 var city = parsed_json['location']['city'];
                 var temp_f = parsed_json['current_observation']['temp_f'];
-                var image = parsed_json['current_observation']['icon_url'];
+                //var image = parsed_json['current_observation']['icon_url'];
+                var icondesc = parsed_json['current_observation']['icon'];
+                var image = "http://icons.wxug.com/i/c/i/" + icondesc + ".gif";
+
                 $('.weather').append(temp_f + "&deg; F");
                 $('#weather_icon').attr("src", image);
+                // $('.icon_desc').append(icondesc);
                 $('.city').append(city);
             }
         });
