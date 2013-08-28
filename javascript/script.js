0f93fc99749f950bdd83fc96d4eb8395256c43ae
oburstein <oburstein@gmail.com>
2013-08-28 13:54:47 -0700

    another scroll block attempt
    also uncommented out coffin script
    added state to city line
    json dot notations

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,6 @@
 
 $(document).ready (function() {
-    //another attempt to block scroll
-
-    
+       
     $(".page").append('<img class="bg" src="images/sunset.jpg" alt="bg">').hide().fadeIn(1000);
 
     $('.title').append("Local Weather").hide().fadeTo(1500, 1);
@@ -13,23 +11,23 @@ $(document).ready (function() {
             url: "http://api.wunderground.com/api/dfc805df5d6fe1d4/geolookup/conditions/q/" + location + ".json",
             dataType: "jsonp",
             success: function (parsed_json) {
-                var city = parsed_json['location']['city'];
-                var temp_f = parsed_json['current_observation']['temp_f'];
-                //var image = parsed_json['current_observation']['icon_url'];
-                var icondesc = chooseIcon(parsed_json['current_observation']['icon']);
-                
-                
-                $('.weather').append(temp_f + "&deg; F").hide().fadeTo(1500, 1);
+                var city = parsed_json.location.city;
+                var state = parsed_json.location.state;
+                var temp_f = parsed_json.current_observation.temp_f;
+                var icondesc = chooseIcon(parsed_json.current_observation.icon);
+
+                $('.weather').append(temp_f +"&deg;F").hide().fadeTo(1500, 1);
                 $('.icon').append(icondesc).hide().fadeTo(1500, 1);
-                //var image = "http://icons.wxug.com/i/c/i/" + icondesc + ".gif";
-                //$('#weather_icon').attr("src", image).hide().fadeTo(1500, 1);
-                // $('.icon_desc').append(icondesc);
-                $('.city').append(city).hide().fadeTo(1500, 1);
+                $('.city').append(city + ", " + state).hide().fadeTo(1500, 1);
             }
         });
         
     });
 });
+
+function blockMove() {
+  event.preventDefault() ;
+}
 
 function chooseIcon(conditions) {
     switch(conditions) {
@@ -76,3 +74,10 @@ function chooseIcon(conditions) {
         return ")";
     }
 }
+
+
+
+                //var image = "http://icons.wxug.com/i/c/i/" + icondesc + ".gif";
+                //$('#weather_icon').attr("src", image).hide().fadeTo(1500, 1);
+                // $('.icon_desc').append(icondesc);
+                 //var image = parsed_json['current_observation']['icon_url'];
