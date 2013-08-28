5e47562a1f29324461cf87ed58cbfd73755fa072
oburstein <oburstein@gmail.com>
2013-08-28 10:45:47 -0700

    removing height/width for div

--- javascript/script.js
+++ javascript/script.js
@@ -13,11 +13,12 @@ $(document).ready (function() {
                 var city = parsed_json['location']['city'];
                 var temp_f = parsed_json['current_observation']['temp_f'];
                 //var image = parsed_json['current_observation']['icon_url'];
-                var icondesc = parsed_json['current_observation']['icon'];
-                var image = "http://icons.wxug.com/i/c/i/" + icondesc + ".gif";
+                var icondesc = chooseIcon(parsed_json['current_observation']['icon']);
+                
                 
                 $('.weather').append(temp_f + "&deg; F").hide().fadeTo(1500, 1);
-                $('.icon').append("B").hide().fadeTo(1500, 1);
+                $('.icon').append(icondesc).hide().fadeTo(1500, 1);
+                //var image = "http://icons.wxug.com/i/c/i/" + icondesc + ".gif";
                 //$('#weather_icon').attr("src", image).hide().fadeTo(1500, 1);
                 // $('.icon_desc').append(icondesc);
                 $('.city').append(city).hide().fadeTo(1500, 1);
@@ -27,3 +28,48 @@ $(document).ready (function() {
     });
 });
 
+function chooseIcon(conditions) {
+    switch(conditions) {
+    case 'clear':
+      return "B";     
+      break;
+    case 'cloudy':
+      return "N";      
+      break;
+    case 'flurries':
+      return "U";      
+      break;
+    case 'fog':
+      return "L";      
+      break;
+    case 'hazy':
+      return "M";      
+      break;
+    case 'mostlycloudy':
+      return "H";      
+      break;
+    case 'mostlysunny':
+      return "H";  
+      break;
+    case 'partlycloudy': //same as mostly sunny
+      return "H";  
+      break;
+    case 'partlysunny': //same as mostly cloudy
+      return "H";  
+      break;
+    case 'rain':
+      return "R";  
+      break;
+    case 'snow':
+      return "X";  
+      break;
+    case 'sunny': //same as mostly cloudy
+      return "B";  
+      break;
+    case 'tstorms':
+      return "0";  
+      break;                        
+    default:
+        return ")";
+    }
+}
